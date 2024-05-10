41003f1eaad02c158b74838ab12ea1caeea24d0e
dango8989 <xdragonbrx@gmail.com>
2024-05-10 17:00:52 +0900

    テスト完了 ステップ1完成

--- PasswordManager.sh
+++ PasswordManager.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
 #APPRENTICE提出QUEST パスワードマネージャー
-#ユーザーから入力された情報をファイルに書き込む
 
-#パスワードマネージャーへようこそ！
-# サービス名を入力してください：
-# ユーザー名を入力してください：
-# パスワードを入力してください：
-# # 入力が完了したら
-# Thank you!
+# ユーザーから入力された以下の情報をFileNameに保存する
+#  ・サービス名
+#  ・ユーザー名
+#  ・パスワード
 
 
 #データ取得用関数
-#   第一引数：ユーザーに表示するテキスト
-#   第二引数：ユーザーから取得するテキスト
+#   引数
+#   第1引数：ユーザーに表示するテキスト
+#   第2引数：ユーザーから取得するテキスト
 GetData(){
     echo "$1"
     read "$2"
@@ -20,20 +18,25 @@ GetData(){
 
 
 #データ保存用関数
+#   引数
+#   第1~3引数:保存するデータ
+#   第4引数  :保存するファイル名
 AddData(){
-    echo "$1:$2:$3" >> "PassWord.txt"
+    echo "$1:$2:$3" >> "$4"
 }
 
 
 #メイン関数
 Main(){
-    echo "パスワードマネージャーへようこそ！"
+    FileName="PassWord.txt"
 
+    echo "パスワードマネージャーへようこそ！"
     GetData "サービス名を入力してください：" ServiceName
     GetData "ユーザー名を入力してください：" UserName
     GetData "パスワードを入力してください：" PassWord
 
-    AddData 
+
+    AddData $ServiceName $UserName $PassWord $FileName
     echo "Thank you!"
 }
 
